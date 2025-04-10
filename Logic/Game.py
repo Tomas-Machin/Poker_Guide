af32a0f4dfe4e3a52cb646f92f1b166c8a4e45f8
Tomas Machin <tm.esteve@gmail.com>
2025-04-10 15:52:27 +0200

    quitando comentarios

--- Logic/Game.py
+++ Logic/Game.py
@@ -1,16 +1,10 @@
-from Objetcs.Deck import Deck
-from Objetcs.Player import Player
+from Objects.Deck import Deck
+from Objects.Player import Player
 #from Objetcs.Round import Round
-from Objetcs.Table import Table
-from Logic.Validations import Validations
-"""from deck import Deck
-from player import Player
-from table import Table
-from round import Round
-from validations import Validations"""
+from Objects.Table import Table
 
 class PokerGame:
-    def __init__(self, num_players, user_position, blinds, user_hand, players_pockets):
+    def __init__(self, user_position, num_players, blinds, user_hand, players_pockets):
         self.rivals = [Player("Rival", []) for _ in range(num_players - 1)]
         self.user_position = user_position
         self.table = Table(num_players, blinds)
@@ -18,7 +12,6 @@ class PokerGame:
         # me renta usar un objeto Player o uso directament el de Mesa | si pq en el futuro es mejor para los rangos de cartas
         self.players_pockets = players_pockets
         self.deck = Deck()
-        self.validations = Validations(num_players, user_position, self.table.positions, blinds, user_hand, players_pockets)
         self.assign_user_into_positions()
 
     def assign_user_into_positions(self):
@@ -36,16 +29,9 @@ class PokerGame:
                 self.table.poker["Positions"][position]["Chips"] = self.players_pockets[index]
                 #self.table.poker["Positions"][position]["hand"] = [self.deck.draw_card() for _ in range(2)]
 
-    """def deal_community_cards(self,):
-        if(self.round.ronda['Round'] == 1):
-            cards = [self.deck.draw_card() for _ in range(4)].pop(0)
-            self.round = Round(self.round.pot, cards)"""
-
     def start_game(self):
         self.deck.shuffle()
         self.deal_cards_and_assign_money()
-        self.validations.confirm_data()
-        # self.play_rounds()
 
     def game_information(self):
         print("\nCartas del jugador usuario:")
@@ -56,7 +42,3 @@ class PokerGame:
 
         # print("\nPreflop round:")
         # print(self.round.ronda)
-
-    """def play_rounds(self):
-        # Aquí irán las rondas de apuestas y lógica adicional
-        pass"""
