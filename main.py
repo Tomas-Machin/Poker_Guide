31f6eb69a817ef086ed9a70c800a0191cb50e1df
Tomas Machin <tm.esteve@gmail.com>
2025-04-16 13:20:08 +0200

    ajustando y poniendo ganadores en primera ronda

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from Logic.Game import PokerGame
-from Logic.Bayesian_Network import Network
+#from Logic.Algorithms.Bayesian_Network import Network
 
-from Logic.Information import info_registration
+from Logic.Information import info_registration, firstDecision
 
 if __name__ == "__main__":
     # inicio
     user_position, num_players, blinds, user_hand, players_pockets = info_registration()
     game = PokerGame(user_position, num_players, blinds, user_hand, players_pockets)
     game.start_game()
-    game.game_information()
+    pot_in_bets, players_left = firstDecision(num_players, blinds)
+    #game.game_information()
