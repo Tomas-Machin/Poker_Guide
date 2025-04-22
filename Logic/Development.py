a56f05e8c379733e472d5eff6c2708341b859643
Tomas Machin <tm.esteve@gmail.com>
2025-04-22 20:30:18 +0200

    arregalndo las posiciones en las rondas

--- Logic/Development.py
+++ Logic/Development.py
@@ -3,6 +3,9 @@ from Objects.Table.Table import Table
 
 GAME_ROUNDS = ["PREFLOP", "POSTFLOP", "TURN", "RIVER"]
 FALLEN_POT = 0
+positions_arranged = []
+actions_arranged = []
+pot_in_bets_arranged = []
 
 def roundDecisions(num_players, blinds, user_position, players_pockets, user_hand, positions, actions, pot_in_bets):
     if GAME_ROUNDS[0] == 'PREFLOP':
@@ -27,7 +30,7 @@ def playerAction(num_players, blinds, user_position, players_pockets, user_hand,
         
         actions, pot_in_bets, players_left = decisionResult(bet, actions, pot_in_bets, blinds, players_left, i, positions)
                 
-        print(pot_in_bets, ' | ', actions)
+        print(pot_in_bets, ' | ', actions, ' | ', positions)
 
     players_left = num_players - players_left   # solo en la primera ronda -> puedo actualizar la variable num_players
     # print("Jugadores restantes: ", players_left)
@@ -61,11 +64,11 @@ def decisionResult(bet, actions, pot_in_bets, blinds, players_left, index, posit
         if GAME_ROUNDS[0] == "PREFLOP":
             max_bet = max(pot_in_bets)
             # CALL 1º RONDA DE CIEGAS
-            if float(bet) == blinds/2 and max(pot_in_bets) == blinds and positions[index] == 'SB' or float(bet) + blinds/2 == max_bet:
+            if float(bet) == blinds/2 and max(pot_in_bets) == blinds and positions[index] == 'SB': # or float(bet) + blinds/2 == max_bet:
                 pot_in_bets[index] += float(bet)
                 actions[index] = "CALL"
                 print(f"1-Ha calleado la posición: {positions[index]}.")
-            elif float(bet) == 0 and max(pot_in_bets) == blinds and positions[index] == 'BB' or float(bet) + blinds == max_bet:
+            elif float(bet) == 0 and max(pot_in_bets) == blinds and positions[index] == 'BB': # or float(bet) + blinds == max_bet:
                 pot_in_bets[index] += float(bet)
                 actions[index] = "CALL"
                 print(f"2-Ha calleado la posición: {positions[index]}.")
@@ -83,11 +86,13 @@ def decisionResult(bet, actions, pot_in_bets, blinds, players_left, index, posit
             else:
                 exit("La apuesta realizada no es valida.")      
                 # dejar tomar la decision otra vez?    
-        else:
+        else:   
+            # PULIR MUCHO Y VER BIEN Q ES LO Q PASA
             max_bet = 0
             print("Rondas posteriores: ", bet, previous_bets[index], pot_in_bets[index], index)
             # CHECK
-            if bet == 0 and (previous_bets[index - 1] == pot_in_bets[index - 1] or index == 0):     # este if no funciona
+            # if bet == 0 and (previous_bets[index] == pot_in_bets[index] or index == 0):     # este if no funciona
+            if bet == max_bet and previous_bets[index] == pot_in_bets[index]:     # este if no funciona
                 actions[index] = "CHECK"
                 print(f"Ha chequeado la posición: {positions[index]}.")
             elif float(bet) + pot_in_bets[index] > max_bet and bet != 0:
@@ -118,7 +123,9 @@ def roundResult(pot_in_bets, actions, players_left, blinds, user_position, playe
         for i in range(0, len(actions)):
             # print(i, pot_in_bets[i], max(pot_in_bets), actions[i], num_players)
             if pot_in_bets[i] < max(pot_in_bets) and actions[i] != "FOLD":  
-                # FALTA LA RED BAYESIANA
+                if (positions[i] == user_position):
+                    bynet = Network(user_position, players_pockets[user_position], blinds, user_hand, players_left)
+                    bynet.result_network()
                 bet = input(f"Cantidad de apuesta (vacío - FOLD) de la posicion: {positions[i]}: ")
                 actions, pot_in_bets, players_left = decisionResult(bet, actions, pot_in_bets, blinds, players_left, i, positions)
                 print(pot_in_bets, ' | ', actions)
@@ -136,16 +143,42 @@ def roundResult(pot_in_bets, actions, players_left, blinds, user_position, playe
 
 def adjustTable(positions, actions, pot_in_bets):
     global FALLEN_POT
+    global positions_arranged
+    global actions_arranged
+    global pot_in_bets_arranged
     i = 0
+    
 
-    while i < len(positions):
-        if actions[i] == "FOLD":
-            del positions[i]
-            del actions[i]
-            FALLEN_POT += pot_in_bets[i]
-            del pot_in_bets[i]
+    if FALLEN_POT == 0:
+        inicio = positions.index('SB')
+        empaquetado = list(zip(positions, actions, pot_in_bets))
+        reordenado = empaquetado[inicio:] + empaquetado[:inicio]
+        positions_arranged, actions_arranged, pot_in_bets_arranged = zip(*reordenado)
+        positions_arranged = list(positions_arranged)
+        actions_arranged = list(actions_arranged)
+        pot_in_bets_arranged = list(pot_in_bets_arranged)
+
+    while i < len(positions_arranged):
+        if actions_arranged[i] == "FOLD":
+            del positions_arranged[i]
+            del actions_arranged[i]
+            FALLEN_POT += pot_in_bets_arranged[i]
+            del pot_in_bets_arranged[i]
         else:
             i += 1
 
-    return positions, actions, pot_in_bets
+    print(positions_arranged)
+    print(actions_arranged)
+    print(pot_in_bets_arranged)
+
+    # while i < len(positions):
+    #     if actions[i] == "FOLD":
+    #         del positions[i]
+    #         del actions[i]
+    #         FALLEN_POT += pot_in_bets[i]
+    #         del pot_in_bets[i]
+    #     else:
+    #         i += 1
+
+    return positions_arranged, actions_arranged, pot_in_bets_arranged
         
