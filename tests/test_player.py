681f9d6929e65b8ecda7ea4828208f2967580e3f
tmesteve <tm.esteve@gmail.com>
2025-06-24 18:14:48 +0200

    tests para los requisitos de la mano del usuario

--- tests/test_player.py
+++ tests/test_player.py
@@ -16,6 +16,17 @@ class TestPlayer(unittest.TestCase):
     def test_chips_greater_than_zero(self):
         self.assertGreater(self.player.chips, 0)
 
+    def test_hand_has_two_cards(self):
+        self.assertEqual(len(self.player.hand), 2)
+
+    def test_hand_cards_are_different(self):
+        self.assertNotEqual(self.player.hand[0], self.player.hand[1])
+
+    def test_hand_cards_format(self):
+        for card in self.player.hand:
+            self.assertEqual(len(card), 2)
+            self.assertIn(card[0], "23456789TJQKA")
+            self.assertIn(card[1], "HSCD")
 
 if __name__ == '__main__':
     unittest.main()
