bca4256953e882590868fef21089ae9d015d56c4
tmesteve <tm.esteve@gmail.com>
2025-06-24 18:33:24 +0200

    test posiciones para un numero de jugadores determinado

--- tests/test_table.py
+++ tests/test_table.py
@@ -1,1 +1,24 @@
+import unittest
+import json
+from Objects.Table.Table import Table
 
+class TestTable(unittest.TestCase):
+
+    def test_assign_positions_valid_num_players(self):
+        expected_positions = {
+            2: ["SB", "BB"],
+            3: ["BU", "SB", "BB"],
+            4: ["CO", "BU", "SB", "BB"],
+            5: ["HJ", "CO", "BU", "SB", "BB"],
+            6: ["MP", "HJ", "CO", "BU", "SB", "BB"],
+            7: ["UTG", "MP", "HJ", "CO", "BU", "SB", "BB"]
+        }
+        num_players = 5
+        for num_players, positions in expected_positions.items():
+            table = Table(num_players, blinds=0.02)
+            self.assertEqual(table.positions, positions)
+
+    
+
+if __name__ == '__main__':
+    unittest.main()
