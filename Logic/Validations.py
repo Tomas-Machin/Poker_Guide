c814caa6fe92d8597ff091a0fb338936f56953df
tmesteve <tm.esteve@gmail.com>
2025-06-24 20:18:15 +0200

    Refactor de Validations

--- Logic/Validations.py
+++ Logic/Validations.py
@@ -11,35 +11,37 @@ class Validations:
 
     def validate_number_of_players(self):
         if self.num_players < 2 or self.num_players > 7:
-            exit("\nLa cantidad de jugadores no es correcta.\n")
+            raise ValueError("\nLa cantidad de jugadores no es correcta.\n")
 
     def validate_user_position(self):
         if self.user_position not in self.positions:
-            exit("\nLa posición introducida es inválida o no esta en una mesa de ese tamaño.\n")
+            raise ValueError("\nLa posición introducida es inválida o no esta en una mesa de ese tamaño.\n")
 
     def validate_blinds(self):
         if self.blinds < 0.02: # falla y ns porque - if isinstance(blinds, str) or float(blinds) < 0.02:
-            exit("\nLas ciegas introducidas no son válidas.\n")
+            raise ValueError("\nLas ciegas introducidas no son válidas.\n")
 
     def validate_user_hand(self):
         deck = Deck()
         if not isinstance(self.hand, list) or len(self.hand) != 2:
-            exit("La mano debe contener exactamente dos cartas.")
+            raise ValueError("La mano debe contener exactamente dos cartas.")
 
         card1 = self.hand[0].strip().upper()
         card2 = self.hand[1].strip().upper()
 
         if card1 not in deck.cards:
-            exit(f"{card1} no es una carta válida.")
+            raise ValueError(f"{card1} no es una carta válida.")
         if card2 not in deck.cards:
-            exit(f"{card2} no es una carta válida.")
+            raise ValueError(f"{card2} no es una carta válida.")
         if card1 == card2:
-            exit("Las cartas no pueden ser iguales.")
+            raise ValueError("Las cartas no pueden ser iguales.")
 
-    # Hacer q pueda jugar sin las chips -> hacer q por defecto sean 0 *
+    # Hacer q pueda jugar sin las chips -> hacer q por defecto sean 0 * y de ahi calcular
     def validate_chips(self):
         if float(self.players_pockets[self.user_position]) <= 0:
-            exit("\nLas fichas de algun jugador no son válidas.\n")
+            raise ValueError("\nLas fichas de algun jugador no son válidas.\n")
+
+# USO raise ValueError() o exit()
 
 # QUEDAN VALIDACIONES DE LAS APUESTAS E INPUTS DE CADA RONDA
 
