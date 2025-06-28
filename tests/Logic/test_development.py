e6a893a2a290a59bb1c99ce19a952eb5b1646e84
Tomas Machin <tm.esteve@gmail.com>
2025-06-28 11:38:13 +0200

    test de la informacion base de apuestas y acciones de los jugadores antes de empezar

---
+++ tests/Logic/test_development.py
@@ -0,0 +1,17 @@
+import unittest
+from Logic.Development import basePot_ActionsTable, no_call_before_raise
+
+class TestGameDevelopment(unittest.TestCase):
+
+    def test_base_pot_actions_table_5_players(self):
+        num_players = 5
+        blinds = 2
+        pot, round_bets, actions = basePot_ActionsTable(num_players, blinds)
+
+        self.assertEqual(pot, [0, 0, 0, 1.0, 2.0])
+        self.assertEqual(round_bets, [0, 0, 0, 0, 0])
+        self.assertEqual(actions, ['', '', '', '', ''])
+
+if __name__ == '__main__':
+    unittest.main()
+
