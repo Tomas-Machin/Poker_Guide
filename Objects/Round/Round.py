427aa7c30485e950a10353f025993d640d7c4b6a
Tomas Machin <tm.esteve@gmail.com>
2025-10-04 18:53:25 +0200

    editando el bote de la ronda

--- Objects/Round/Round.py
+++ Objects/Round/Round.py
@@ -1,6 +1,6 @@
 class Round:
-    def __init__(self, pot, community_cards, blinds, playersInRound):
-        self.pot = "{0:.2f}".format(sum(pot) + blinds + blinds / 2)
+    def __init__(self, community_cards, blinds, playersInRound):
+        self.pot = "{0:.2f}".format(blinds + blinds / 2)
 
         self.ronda = {
             "Fase": "PREFLOP",
