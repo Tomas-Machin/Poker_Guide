67a1e241deb145f9c4c336718a60832232c8fc7f
Tomas Machin <tm.esteve@gmail.com>
2025-10-04 19:14:46 +0200

    editando aun mas el objeto round

--- Objects/Round/Round.py
+++ Objects/Round/Round.py
@@ -1,11 +1,9 @@
 class Round:
     def __init__(self, community_cards, blinds, playersInRound):
-        self.pot = "{0:.2f}".format(blinds + blinds / 2)
-
         self.ronda = {
             "Fase": "PREFLOP",
             "PlayersLeft": playersInRound,
-            "Pot": self.pot,
+            "Pot": "{0:.2f}".format(blinds + blinds / 2),
             "SidePot": [],
             "CommunityCards": community_cards
         }
